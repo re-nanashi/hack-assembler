f2a1f32b8e99573aff62cd3853c0874414d985cb
re-nanashi <jfabro3124@gmail.com>
2022-10-02 21:11:57 +0800

    init

---
+++ parser.h
@@ -0,0 +1,12 @@
+
+enum command_type { A_COMMAND, C_COMMAND, L_COMMAND };
+
+/**
+ * NOTE: Do not ret L_COMMAND for now
+ *       create lexer
+ **/
+enum command_type
+parse_command_type(const char *str_to_parse)
+{
+}
+
