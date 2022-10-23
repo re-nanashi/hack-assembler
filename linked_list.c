f18605d88ae78fec6f1eb1daf8a874db46826587
re-nanashi <jfabro3124@gmail.com>
2022-10-23 19:38:15 +0800

    refactor: Accomodate typedef struct removal

--- linked_list.c
+++ linked_list.c
@@ -5,20 +5,21 @@
 #include "linked_list.h"
 #include "hash_table.h"
 
-linked_list *
+struct linked_list *
 allocate_list()
 {
         /* allocates memory for a linked list pointer */
-        linked_list *list = (linked_list *)malloc(sizeof(linked_list));
+        struct linked_list *list =
+            (struct linked_list *)malloc(sizeof(struct linked_list));
         return list;
 }
 
-linked_list *
-linkedlist_insert(linked_list *list, ht_item *item)
+struct linked_list *
+linkedlist_insert(struct linked_list *list, struct ht_item *item)
 {
         /* inserts the item into the linked list */
         if (!list) {
-                linked_list *head = allocate_list();
+                struct linked_list *head = allocate_list();
                 head->item = item;
                 head->next = NULL;
                 list = head;
@@ -26,7 +27,7 @@ linkedlist_insert(linked_list *list, ht_item *item)
         }
 
         else if (list->next == NULL) {
-                linked_list *node = allocate_list();
+                struct linked_list *node = allocate_list();
                 node->item = item;
                 node->next = NULL;
                 list->next = node;
@@ -34,12 +35,12 @@ linkedlist_insert(linked_list *list, ht_item *item)
         }
 
         /* iterate the list until the tail of the list */
-        linked_list *tmp = list;
+        struct linked_list *tmp = list;
         while (tmp->next->next) {
                 tmp = tmp->next;
         }
 
-        linked_list *node = allocate_list();
+        struct linked_list *node = allocate_list();
         node->item = item;
         node->next = NULL;
         tmp->next = node;
@@ -47,22 +48,22 @@ linkedlist_insert(linked_list *list, ht_item *item)
         return list;
 }
 
-ht_item *
-linkedlist_remove(linked_list *list)
+struct ht_item *
+linkedlist_remove(struct linked_list *list)
 {
         /* removes the head from the linked_list */
         /* and returns the removed item */
         if (!list) return NULL;
         if (!list->next) return NULL;
 
-        linked_list *node = list->next;
-        linked_list *tmp = list;
+        struct linked_list *node = list->next;
+        struct linked_list *tmp = list;
         tmp->next = NULL;
         list = node;
 
         /* copy the contents of the head of the list to item */
-        ht_item *item = NULL;
-        memcpy(item, tmp->item, sizeof(ht_item));
+        struct ht_item *item = NULL;
+        memcpy(item, tmp->item, sizeof(struct ht_item));
 
         /* free memory */
         free(tmp->item->key);
@@ -74,9 +75,9 @@ linkedlist_remove(linked_list *list)
 }
 
 void
-free_linkedlist(linked_list *list)
+free_linkedlist(struct linked_list *list)
 {
-        linked_list *tmp;
+        struct linked_list *tmp;
         while (list != NULL) {
                 tmp = list;
                 list = list->next;
