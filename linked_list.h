74a0868b2a719d14f6cf4db10a764aedfbacc90d
re-nanashi <jfabro3124@gmail.com>
2022-10-23 19:37:45 +0800

    refactor: Remove typedef declarations

--- linked_list.h
+++ linked_list.h
@@ -7,11 +7,11 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+//TODO
 /* forward declaration */
-typedef struct ht_item ht_item;
+struct ht_item;
 
 /* @brief The linked_list definition */
-typedef struct linked_list linked_list;
 struct linked_list {
     /* the hash table item */
     ht_item *item;
@@ -23,7 +23,7 @@ struct linked_list {
  *
  * @return a linked list 
  */
-linked_list *allocate_list();
+struct linked_list *allocate_list();
 
 /**
  * @brief Insert hash table item to a list
@@ -32,7 +32,7 @@ linked_list *allocate_list();
  * @param ht_item the hash table item to be inserted
  * @return the linked list with the added node
  */
-linked_list *linkedlist_insert(linked_list *list, ht_item *item);
+struct linked_list *linkedlist_insert(struct linked_list *list, ht_item *item);
 
 /**
  * @brief Removes the head hash table item node from the list
@@ -42,13 +42,13 @@ linked_list *linkedlist_insert(linked_list *list, ht_item *item);
  * @param linked_list the original list
  * @return the ht_item that was removed from the list 
  */
-ht_item* linkedlist_remove(linked_list* list);
+struct ht_item* linkedlist_remove(struct linked_list* list);
 
 /**
  * @brief Frees the linked list
  *
  * @param linked_list the original list to be freed
  */
-void free_linkedlist(linked_list* list);
+void free_linkedlist(struct linked_list* list);
 
 #endif /* LINKED_LIST_H */
