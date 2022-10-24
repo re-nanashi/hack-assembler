e234ea292885cc651d085008501df8603f2ce851
re-nanashi <jfabro3124@gmail.com>
2022-10-24 23:46:47 +0800

    wip: init parser functions

--- parser.h
+++ parser.h
@@ -1,12 +1,32 @@
+/**
+ * @file parser.h
+ * @author re-nanashi
+ * @brief Parser public functions and data types
+ */
 
+#ifndef PARSER_H
+#define PARSER_H
+
+/* forward declaration */
+struct token;
+
+/* @brief Hack command types */
 enum command_type { A_COMMAND, C_COMMAND, L_COMMAND };
 
 /**
- * NOTE: Do not ret L_COMMAND for now
- *       create lexer
- **/
-enum command_type
-parse_command_type(const char *str_to_parse)
-{
-}
+ * @brief Parses the asm command token stream
+ *
+ * @param tk_stream the token stream to parse
+ * @return the command type of the asm command
+ */
+enum command_type parse(struct token *tk_stream);
 
+/**
+ * @brief Print the command type
+ *
+ * @param ct integer representation of command type enum
+ * @return the command type as a string
+ */
+const char *print_command_type(int ct);
+
+#endif /* PARSER_H */
