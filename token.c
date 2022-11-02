fb354c3080fcbfe077cf4d77692a34b67d6afab3
re-nanashi <jfabro3124@gmail.com>
2022-11-02 12:12:34 +0800

    token init

--- token.c
+++ token.c
@@ -1,11 +1,21 @@
 #include "token.h"
 
-token_T *
-allocate_token(char *value, int type)
+struct token_T *
+allocate_token(enum TokenType type, char *value)
 {
-        token_T *token = calloc(1, sizeof(struct TOKEN_STRUCT));
-        token->value = value;
+        struct token_T *token = calloc(1, sizeof(struct token_T));
+        token->svalue = value;
         token->type = type;
 
         return token;
+}
+
+struct token_T *
+new_token(struct token_T **token, enum TokenType type, char *value)
+{
+        struct token_T *node = allocate_token(type, value);
+        struct token_T *head = *token;
+        head->next = node;
+
+        head = head->next;
 }
--- token.h
+++ token.h
@@ -1,23 +1,47 @@
+/**
+ * @file token.h
+ * @author re-nanashi
+ * @brief Token public functions and data types
+ */
+
 #ifndef TOKEN_H
 #define TOKEN_H
-typedef struct TOKEN_STRUCT 
+
+/* @brief Types of token */
+enum TokenType {
+        /* single-character tokens */
+        TK_LPAREN = 0,
+        TK_RPAREN,
+        TK_OPERATOR, 
+        TK_A_OP,
+        TK_EQUALS,
+        TK_SEMI, 
+        /* literals */
+        TK_INT, 
+        TK_ID,
+        /* keywords */
+        TK_RESERVED, 
+        TK_RESERVED_JMP, 
+        TK_RESERVED_DEST, 
+        /* end-of-command */
+        TK_EOC, 
+};
+
+/* @brief Token struct */
+struct token_T 
 {
-    char *value;
-    enum
-    {
-        TOKEN_RESERVED,
-        TOKEN_ID,
-        TOKEN_LPAREN,
-        TOKEN_RPAREN,
-        TOKEN_A_OP,
-        TOKEN_EQUALS,
-        TOKEN_SEMI, 
-        TOKEN_OPERATOR, 
-        TOKEN_INT, 
-        TOKEN_EOC, 
-    } type;
-} token_T;
+    /* the type of the current token */
+    enum TokenType type;
+    /* the string value */
+    char *svalue;
+    /* the integer value */
+    int ivalue;
+    /* the proceeding token */
+    struct token_T *next;
+};
 
-token_T *allocate_token(char *value, int type);
+struct token_T *allocate_token(enum TokenType type, char *value);
 
-#endif
+struct token_T *new_token(struct token_T *token, enum TokenType type, char *value);
+
+#endif /* TOKEN_H */
