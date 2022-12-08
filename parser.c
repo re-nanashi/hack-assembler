b5bf351424264d198d85f6c0186f06b1df7f57c4
re-nanashi <jfabro3124@gmail.com>
2022-12-09 03:25:23 +0800

    Remove TK_RESERVED condition in is_asymb var inside __is_acommand function

--- parser.c
+++ parser.c
@@ -1,3 +1,8 @@
+/**
+ * NOTE: Parser assumes correctly formed input.
+ *       Does little to no error checking.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -5,12 +10,6 @@
 
 #include "parser.h"
 #include "lexer.h"
-
-/**
- * NOTE:
- *      Parser assumes correctly formed input.
- *      Does little to no error checking.
- */
 
 static bool
 __is_acommand(const struct token *tok)
@@ -21,8 +20,7 @@ __is_acommand(const struct token *tok)
 
         bool is_acommand_op =
             op->kind == TK_OPERATOR && (strcmp(op->str, "@") == 0);
-        bool is_asymb = symb->kind == TK_RESERVED || symb->kind == TK_ID
-                        || symb->kind == TK_INT;
+        bool is_asymb = symb->kind == TK_ID || symb->kind == TK_INT;
 
         return (is_acommand_op && is_asymb && at_eoc(eoc));
 }
@@ -44,9 +42,7 @@ __is_lcommand(const struct token *tok)
         return is_lcommand_open_op && is_lsymbol && is_lcommand_end_op;
 }
 
-/**
- * Assume correct input.
- */
+/* Assume correct input. */
 enum command_type
 parser_parse(struct token *tk_stream)
 {
@@ -55,11 +51,9 @@ parser_parse(struct token *tk_stream)
         if (__is_acommand(tk_stream)) {
                 ret = A_COMMAND;
         }
-
         else if (__is_lcommand(tk_stream)) {
                 ret = L_COMMAND;
         }
-
         else {
                 ret = C_COMMAND;
         }
@@ -77,9 +71,9 @@ print_command_type(int ct)
 }
 
 const char *
-get_symb_mnemonic(const struct token *const command_tk)
+get_symb_mnemonic(const struct token *const tok)
 {
-        return command_tk->next->str;
+        return tok->next->str;
 }
 
 const char *
