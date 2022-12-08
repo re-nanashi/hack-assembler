9d1b256d4eb50de0d69a453881d27e6344114632
re-nanashi <jfabro3124@gmail.com>
2022-12-09 06:08:34 +0800

    chore: Accomodate change of spelling from token_type_t parameters

---
+++ token.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "token.h"
+
+struct token *
+allocate_token(void)
+{
+
+        struct token *tok = (struct token *)calloc(1, sizeof(struct token));
+
+        /* verify allocation */
+        if (tok == NULL) {
+                printf("Error: allocation error encountered.\n");
+                exit(-1);
+        }
+
+        return tok;
+}
+
+struct token *
+new_token(token_type_t tok_type, struct token *cur_tok, char *str)
+{
+        /* allocate token to memory */
+        struct token *tok = allocate_token();
+        tok->str = (char *)malloc(strlen(str) + 1);
+
+        tok->type = tok_type;
+        strcpy(tok->str, str);
+        cur_tok->next = tok;
+
+        return tok;
+}
+
+bool
+token_is_eoc(struct token const *cur_tok)
+{
+        return TOKEN_EOC == cur_tok->type;
+}
